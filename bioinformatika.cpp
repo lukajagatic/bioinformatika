0a7eba261cbd39e6fc75316c7160db4ac52ccc0c
System Manager <manager@bl8vbox.unassigned-domain>
2017-01-02 20:31:57 +0000

    testbranch

--- bioinformatika.cpp
+++ bioinformatika.cpp
@@ -15,8 +15,13 @@
 #include <stdlib.h>
 #include <stdexcept>
 #include <fstream>
+#include <algorithm>
+#include <map>
 
 using namespace std;
+
+
+map<int,float> mapa;
 
 void split(const std::string &s, char delim, std::vector<std::string> &elems) {
 	std::stringstream ss;
@@ -33,20 +38,32 @@ vector<std::string> split(string &s, char delim) {
 	return elems;
 }
 
-void read(string input_file, float error_treshold) {
+void read(string input_file, float error_threshold) {
 	ifstream myfile(input_file.c_str());
 	string line;
-
 	if (myfile.is_open()) {
 
-		int i = 0;
 
+		vector<float> largest_error;
+		int v = 0;
 		while (getline(myfile, line)) {
-			i++;
+
 			vector<string> tokens = split(line, ' ');
-			if (atof(tokens[2].c_str()) > error_treshold) {
-				cout << tokens[2].c_str() << '\n';
+			if(v == 0){
+				v = atoi(tokens[0].c_str());
 			}
+			if(atoi(tokens[0].c_str()) != v){
+
+
+				sort(largest_error.begin(), largest_error.end());
+				mapa[v]=largest_error[0];
+				largest_error.clear();
+				v = atoi(tokens[0].c_str());
+
+			}
+				largest_error.push_back(atof(tokens[2].c_str()));
+
+
 
 		}
 		myfile.close();
@@ -62,36 +79,29 @@ int main(int argc, char *argv[]) {
 	double start = clock();
 
 	string input_file = "";
-	string output_file = "";
+	float error_threshold = 0;
 	for (unsigned int i = 0; i < argc; i++) {
 		if (i == 1) {
 			input_file += argv[i];
 			if (input_file.substr(input_file.find_last_of(".") + 1) != "mhap")
 				throw runtime_error("Input mhap files allowed");
 		}
-		if (i > 1)
+		if (i == 2) {
+			error_threshold = atof(argv[i]);
+			if (error_threshold>1)
+				throw runtime_error("Error treshold cannot be greather than 1!");
+		}
+		if (i > 2)
 			throw runtime_error("Too many arguments");
 	}
 
-	double lap1 = clock();
-	double lap2 = clock();
-	read(input_file, 0.7);
+	read(input_file, error_threshold);
 
-	// kraj mjerenja
-	double end = clock();
-	//ispis mjerenja
-	cout << "\nTrajanja programa:" << std::endl;
-	cout << "Ucitavanje i edge removing: "
-			<< (double) (lap1 - start) / (double) CLOCKS_PER_SEC << " s"
-			<< std::endl;
-	cout << "Chunker: " << (double) (lap2 - lap1) / (double) CLOCKS_PER_SEC
-			<< " s" << std::endl;
-	cout << "Ispis rezultata u datoteke: "
-			<< (double) (end - lap2) / (double) CLOCKS_PER_SEC << " s"
-			<< std::endl;
-	cout << "Ukupno vrijeme: "
-			<< (double) (double) (end - start) / (double) CLOCKS_PER_SEC << " s"
-			<< std::endl;
+	for(map<int, float >::const_iterator it = mapa.begin();
+	    it != mapa.end(); ++it)
+	{
+		std::cout << it->first << " " << it->second << "\n";
+	}
 
 	exit(0);
 
